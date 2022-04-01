ad1fbe296bec091af10fa4ce1aab2f59c308de05
Evaldo Santos <jesantosjr@protonmail.com>
2022-03-31 21:59:22 -0300

    feat: list pokemons from a list

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Pokemon } from '../../model/pokemon';
+import { Type } from '../../model/type';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.sass']
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  pokemons: Pokemon[] = [
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: 'Bulbasaur',
+      types: [
+        Type.Grass,
+        Type.Poison
+      ]
+    }
+  ];
 
 }
